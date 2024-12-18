27764e3d833a627ea3886012ddd92ee645978da7
varun-dubagunta <varun.dubagunta@gmail.com>
2024-12-18 01:33:13 -0600

    Implemented linked list class

--- code/sllinkedlist/linkedlist.cpp
+++ code/sllinkedlist/linkedlist.cpp
@@ -1,13 +1,99 @@
 #include <iostream>
 #include "node.h"
 
+class Linkedlist{
+    private:
+    Node* head;
+
+    public:
+    Linkedlist(Node* head): head(head){} //constructor with params
+
+    Linkedlist(): head(nullptr){} //default constructor
+    
+    ~Linkedlist(){
+        Node* temp = head;
+        while(temp!= nullptr){
+            Node* temp2 = temp;
+            temp = temp->getNext();
+            delete temp2;
+        }
+
+    }
+    Node* getHead() const{
+        return head;
+    }
+    bool isEmpty(){
+        return head == nullptr;
+    }
+
+    void append(Node* newNode){ //TODO: append function that takes node as parameter
+         if(isEmpty()){
+            head = newNode;
+            return;
+        }
+
+        Node* temp = this->head;
+
+        while(temp->hasNext()){
+            temp = temp->getNext();
+        }
+        temp->setNext(newNode);
+    }
+    void append(int value){ //TODO: append function that takes a value as a parameter
+       
+        Node* newNode = new Node(value);
+
+         if(isEmpty()){
+            head = newNode;
+            return;
+        }
+
+        Node* temp = this->head;
+        while(temp->hasNext()){
+            temp = temp->getNext();
+        }
+        temp->setNext(newNode);
+
+    }
+
+    void insert(int pos, Node* newNode){ //TODO: insert Node pos iterations away from head
+        if(isEmpty()){
+            this->head = newNode;
+            return;
+        }
+
+        Node* temp = this->head;
+        
+        int n = 0;
+        while(n<pos){
+            temp = temp->getNext();
+            n++;
+        }
+        newNode->setNext(temp->getNext());
+        temp->setNext(newNode);
+    }
+
+    void insert(unsigned int pos, int value){ //TODO: insert Node pos iterations away from head given value of node
+        Node* newNode = new Node(value);
+        
+        if(isEmpty()){
+            this->head = newNode;
+            return;
+        }
+
+        Node* temp = this->head;
+        int n = 0;
+        while(n<pos){
+            temp = temp->getNext();
+            n++;
+        }
+        newNode->setNext(temp->getNext());
+        temp->setNext(newNode);
+    }
+    
+
+};
 
 int main(){
-    Node node(3);
-    Node* n2 = new Node(5);
-    node.setNext(n2);
-
-    std::cout<<node.getNext()->getValue()<<std::endl;
-    delete n2;
-    return 0;
+    
 }
